08e4b921cb5244ec93155f4dd441c33a28094181
Suryakant Singh <surykntsingh@gmail.com>
2026-03-30 20:34:14 -0400

    updated models

--- modules/tester_AllinOne.py
+++ modules/tester_AllinOne.py
@@ -2,11 +2,11 @@ import logging
 import os
 from abc import abstractmethod
 
-import cv2
+# import cv2
 import pandas as pd
 import torch
 
-from modules.utils import generate_heatmap
+# from modules.utils import generate_heatmap
 from tqdm import tqdm
 import logging
 
@@ -108,32 +108,32 @@ class Tester(BaseTester):
         return log
 
 
-    def plot(self):
-        assert self.args.batch_size == 1 and self.args.beam_size == 1
-        self.logger.info('Start to plot attention weights in the test set.')
-        os.makedirs(os.path.join(self.save_dir, "attentions"), exist_ok=True)
-        mean = torch.tensor((0.485, 0.456, 0.406))
-        std = torch.tensor((0.229, 0.224, 0.225))
-        mean = mean[:, None, None]
-        std = std[:, None, None]
-
-        self.model.eval()
-        with torch.no_grad():
-            for batch_idx, (images_id, images, reports_ids, reports_masks) in tqdm(enumerate(self.test_dataloader)):
-                images, reports_ids, reports_masks = images.to(self.device), reports_ids.to(
-                    self.device), reports_masks.to(self.device)
-                output = self.model(images, mode='sample')
-                image = torch.clamp((images[0].cpu() * std + mean) * 255, 0, 255).int().cpu().numpy()
-                report = self.model.tokenizer.decode_batch(output.cpu().numpy())[0].split()
-                attention_weights = [layer.src_attn.attn.cpu().numpy()[:, :, :-1].mean(0).mean(0) for layer in
-                                     self.model.encoder_decoder.model.decoder.layers]
-                for layer_idx, attns in enumerate(attention_weights):
-                    assert len(attns) == len(report)
-                    for word_idx, (attn, word) in enumerate(zip(attns, report)):
-                        os.makedirs(os.path.join(self.save_dir, "attentions", "{:04d}".format(batch_idx),
-                                                 "layer_{}".format(layer_idx)), exist_ok=True)
-
-                        heatmap = generate_heatmap(image, attn)
-                        cv2.imwrite(os.path.join(self.save_dir, "attentions", "{:04d}".format(batch_idx),
-                                                 "layer_{}".format(layer_idx), "{:04d}_{}.png".format(word_idx, word)),
-                                    heatmap)
+    # def plot(self):
+    #     assert self.args.batch_size == 1 and self.args.beam_size == 1
+    #     self.logger.info('Start to plot attention weights in the test set.')
+    #     os.makedirs(os.path.join(self.save_dir, "attentions"), exist_ok=True)
+    #     mean = torch.tensor((0.485, 0.456, 0.406))
+    #     std = torch.tensor((0.229, 0.224, 0.225))
+    #     mean = mean[:, None, None]
+    #     std = std[:, None, None]
+    #
+    #     self.model.eval()
+    #     with torch.no_grad():
+    #         for batch_idx, (images_id, images, reports_ids, reports_masks) in tqdm(enumerate(self.test_dataloader)):
+    #             images, reports_ids, reports_masks = images.to(self.device), reports_ids.to(
+    #                 self.device), reports_masks.to(self.device)
+    #             output = self.model(images, mode='sample')
+    #             image = torch.clamp((images[0].cpu() * std + mean) * 255, 0, 255).int().cpu().numpy()
+    #             report = self.model.tokenizer.decode_batch(output.cpu().numpy())[0].split()
+    #             attention_weights = [layer.src_attn.attn.cpu().numpy()[:, :, :-1].mean(0).mean(0) for layer in
+    #                                  self.model.encoder_decoder.model.decoder.layers]
+    #             for layer_idx, attns in enumerate(attention_weights):
+    #                 assert len(attns) == len(report)
+    #                 for word_idx, (attn, word) in enumerate(zip(attns, report)):
+    #                     os.makedirs(os.path.join(self.save_dir, "attentions", "{:04d}".format(batch_idx),
+    #                                              "layer_{}".format(layer_idx)), exist_ok=True)
+    #
+    #                     heatmap = generate_heatmap(image, attn)
+    #                     cv2.imwrite(os.path.join(self.save_dir, "attentions", "{:04d}".format(batch_idx),
+    #                                              "layer_{}".format(layer_idx), "{:04d}_{}.png".format(word_idx, word)),
+    #                                 heatmap)
